65bc569ff46d182ea5a5616d835818a2ee9238c2
zl8244 <zl8244@g.rit.edu>
2024-04-18 11:14:05 -0400

    Changed how ranks are set

--- Card.java
+++ Card.java
@@ -1,14 +1,14 @@
 public class Card {
     
-    private final String rank;
+    private final int rank;
     private final String suit;
 
-    public Card(String _rank, String _suit) {
+    public Card(int _rank, String _suit) {
         rank = _rank;
         suit = _suit;
     }
 
-    public String getRank() {
+    public int getRank() {
         return rank;
     }
 
@@ -17,6 +17,15 @@ public class Card {
     }
 
     public String toString() {
+        if(rank == 11) {
+            return "Jack of " + suit;
+        } else if(rank == 12) {
+            return "Queen of " + suit;
+        } else if(rank == 13) {
+            return "King of " + suit;
+        } else if(rank == 1) {
+            return "Ace of " + suit;
+        }
         return rank + " of " + suit;
     }
 }
--- Poker.java
+++ Poker.java
@@ -1,7 +1,7 @@
 public class Poker {
     public static void main(String[] args) {
         Deck deck = new Deck();
-        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        int[] ranks = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
         String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
         for(int i = 0; i < ranks.length; i++) {
             for(int j = 0; j < suits.length; j++) {
