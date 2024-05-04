96c353e2460b51cd312bbb3cf3349ea4c8447cf7
zl8244 <zl8244@g.rit.edu>
2024-05-03 22:34:53 -0400

    Revamped shuffling and sorting

--- Deck.java
+++ Deck.java
@@ -23,6 +23,7 @@ public class Deck {
     }
 
     public void shuffle() {
+        
         for(int i = 0; i < deckofCards.size(); i++) {
             int randIndex1 = (int)(Math.random()*51);
             int randIndex2 = (int)(Math.random()*51);
@@ -31,13 +32,62 @@ public class Deck {
             }
             swapCards(randIndex1, randIndex2);
         }
-    }
-
-    public void resetDeck() {
+        
+        ArrayList<Card> left = new ArrayList<>();
+        ArrayList<Card> right = new ArrayList<>();
+        for(int i = 0; i < deckofCards.size()/2; i++) {
+            left.add(deckofCards.get(i));
+        }
+        for(int i = deckofCards.size()/2; i < deckofCards.size(); i++) {
+            right.add(deckofCards.get(i));
+        }
+        ArrayList<Card> temp = new ArrayList<>();
+        for(int i = 0; i < left.size(); i++) {
+            temp.add(left.get(i));
+            temp.add(right.get(i));
+        }
         for(int i = 0; i < deckofCards.size(); i++) {
-            deckofCards.set(i, sortedDeck.get(i));
+            deckofCards.set(i, temp.get(i));
         }
     }
+
+    public void sortDeck(String[] suitOrder) {
+        // Sort by rank
+        for (int i = 0; i < deckofCards.size(); i++) {
+            for (int j = i + 1; j < deckofCards.size(); j++) {
+                Card card1 = deckofCards.get(i);
+                Card card2 = deckofCards.get(j);
+                if (card1.getRank() > card2.getRank()) {
+                    swapCards(i, j);
+                }
+            }
+        }
+    
+        // Sort by suit order within groups of 4 cards
+        for (int i = 0; i < deckofCards.size() - 3; i += 4) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = j + 1; k < 4; k++) {
+                    Card card1 = deckofCards.get(i + j);
+                    Card card2 = deckofCards.get(i + k);
+                    int suit1Index = getSuitIndex(card1.getSuit(), suitOrder);
+                    int suit2Index = getSuitIndex(card2.getSuit(), suitOrder);
+                    if (suit1Index > suit2Index) {
+                        swapCards(i + j, i + k);
+                    }
+                }
+            }
+        }
+    }
+
+    private int getSuitIndex(String suit, String[] suitOrder) {
+        for (int i = 0; i < suitOrder.length; i++) {
+            if (suitOrder[i].equals(suit)) {
+                return i;
+            }
+        }
+        return -1; // Suit not found
+    }
+    
 
     public Card dealCard() {
         return deckofCards.remove(0);
